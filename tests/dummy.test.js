c582f0739ad76903e81ce0281442ca7e2230eae3
Anthony-Phillip-Collins <anthony.phillip.collins@gmail.com>
2022-09-29 11:57:15 +1000

    4.5*: helper functions and unit tests, step3

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,8 +1,21 @@
 const listHelper = require('../utils/list_helper');
+const dummyBlogs = require('./dummyBlogs');
 
-test('dummy returns one', () => {
-  const blogs = [];
+describe('Blogs', () => {
+  test('dummy returns one', () => {
+    const blogs = [];
 
-  const result = listHelper.dummy(blogs);
-  expect(result).toBe(1);
+    const result = listHelper.dummy(blogs);
+    expect(result).toBe(1);
+  });
+
+  test('total likes', () => {
+    const result = listHelper.totalLikes(dummyBlogs);
+    expect(result).toBe(36);
+  });
+
+  test('favourite blog', () => {
+    const result = listHelper.favoriteBlog(dummyBlogs);
+    expect(result).toStrictEqual({ title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', likes: 12 });
+  });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,21 @@
 const dummy = (blogs) => (blogs ? 1 : 0);
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, { likes }) => sum + likes;
+  return blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const reducer = (blogA, blogB) => {
+    const prev = blogA || blogB;
+    const { title, author, likes } = blogB.likes > prev.likes ? blogB : prev;
+    return { title, author, likes };
+  };
+  return blogs.reduce(reducer);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
