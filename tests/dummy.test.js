54d5e48a242199ad05f137a983a998e5d6118fe1
Anthony-Phillip-Collins <anthony.phillip.collins@gmail.com>
2022-10-04 13:24:32 +1000

    4.9*: Blog list tests, step2

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -8,34 +8,34 @@ const Blog = require('../models/blog');
 const api = supertest(app);
 
 describe('Blogs', () => {
-  // test('dummy returns one', () => {
-  //   const blogs = [];
+  test('dummy returns one', () => {
+    const blogs = [];
 
-  //   const result = listHelper.dummy(blogs);
-  //   expect(result).toBe(1);
-  // });
+    const result = listHelper.dummy(blogs);
+    expect(result).toBe(1);
+  });
 
-  // test('total likes', () => {
-  //   const result = listHelper.totalLikes(dummyBlogs);
-  //   expect(result).toBe(36);
-  // });
+  test('total likes', () => {
+    const result = listHelper.totalLikes(dummyBlogs);
+    expect(result).toBe(36);
+  });
 
-  // test('favourite blog', () => {
-  //   const result = listHelper.favoriteBlog(dummyBlogs);
-  //   expect(result).toStrictEqual({ title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', likes: 12 });
-  // });
+  test('favourite blog', () => {
+    const result = listHelper.favoriteBlog(dummyBlogs);
+    expect(result).toStrictEqual({ title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', likes: 12 });
+  });
 
-  // test('most blogs', () => {
-  //   const result = listHelper.mostBlogs(dummyBlogs);
-  //   expect(result).toStrictEqual({ author: 'Robert C. Martin', blogs: 3 });
-  // });
+  test('most blogs', () => {
+    const result = listHelper.mostBlogs(dummyBlogs);
+    expect(result).toStrictEqual({ author: 'Robert C. Martin', blogs: 3 });
+  });
 
-  // test('most likes', () => {
-  //   const result = listHelper.mostLikes(dummyBlogs);
-  //   expect(result).toStrictEqual({ author: 'Edsger W. Dijkstra', likes: 17 });
-  // });
+  test('most likes', () => {
+    const result = listHelper.mostLikes(dummyBlogs);
+    expect(result).toStrictEqual({ author: 'Edsger W. Dijkstra', likes: 17 });
+  });
 
-  test('blogs are returned as json', async () => {
+  test('correct amount of blogs', async () => {
     await api
       .get('/api/blogs')
       .expect('Content-Type', /json/)
@@ -45,10 +45,21 @@ describe('Blogs', () => {
     expect(blogs.length).toStrictEqual(dummyBlogs.length);
   });
 
+  test('blog has id', async () => {
+    const blog = await Blog.findOne();
+    expect(blog.id).toBeDefined();
+  });
+
   beforeEach(async () => {
     await Blog.deleteMany({});
 
-    const promises = dummyBlogs.map((blog) => new Blog(blog).save());
+    const promises = [];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const blog of dummyBlogs) {
+      // eslint-disable-next-line no-await-in-loop
+      promises.push(await new Blog(blog).save());
+    }
 
     await Promise.all(promises);
   });
